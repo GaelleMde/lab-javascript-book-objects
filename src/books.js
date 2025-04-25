9e9499565aeb4c46e50e7bf8ceef1175d6bd773d
Gaelle <gaelle.madelaine@gmail.com>
2025-04-25 18:14:40 +0200

    Solved lab

--- src/books.js
+++ src/books.js
@@ -38,15 +38,50 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+{title: "The Old Man and the Sea",
+  pages: 128,
+  author: "Ernest Hemingway",
+  details: {
+  language: "English",
+  description: "One of Hemingway's most famous works, it tells the story of Santiago...",
+   }},
+
+  {title: "The Airbnb Story",
+  pages: 256,
+  author: "Leight Gallagher",
+  details: {
+     language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+    }},
+
+{ title: "Educated - A Memoir",
+pages: 352,
+author: "Tara Westover",
+details: {
+  language: "English",
+  description: "Educated is an account of the struggle for self-invention...",
+}},
+
+
+{ title: "The Art of Learning",
+pages: 288,
+author: "Josh Waitzkin",
+details: {
+language: "English",
+ description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
+}}
+];
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
+// STRING : TITLE - AUTHOR - PAGES pages
 
+ return `${book.title} - ${book.author} - ${book.pages} pages`
 }
 
 
@@ -55,11 +90,24 @@ function getBookDetails() {
 // Your code here:
 
 
+for (let index in booksArray) {
+  delete booksArray[index].details.language
+}
+
+console.log(booksArray)
+
+
+
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+for (let index2 in booksArray) {
+  let valorReadingTime  = Math.ceil((booksArray[index2].pages *  500) / 90)
+  booksArray[index2].readingTime = valorReadingTime
+}
 
+console.log(booksArray)
 
 
 
